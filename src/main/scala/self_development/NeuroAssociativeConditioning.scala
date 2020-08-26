9489351783a6afbb719a847b0b3caf8ab516a5df
kuberlog <collinalexbell@gmail.com>
2020-08-25 20:42:33 -0500

    init project with NeuroAssociativeConditioning

---
+++ src/main/scala/self_development/NeuroAssociativeConditioning.scala
@@ -0,0 +1,12 @@
+package self_development
+
+object NeuroAssociativeConditioning {
+  val steps = List(
+    "Decide what you want and what is preventing you from having it now",
+    "Get leverage by thinking of how making change will be pleasurable and how not changing will cause pain",
+    "Identify and interrup tht limiting pattern by using shock or confusion",
+    "Create a new empowering alternative that provides similar benifits as the original pattern",
+    "Condition the change using rewards after taking action",
+    "Test the behaviour for effectiveness and ecology"
+  )
+}
